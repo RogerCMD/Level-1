d31be725c31f155e9539fdd8087b467e69f178d3
RogerCMD <74694552+RogerCMD@users.noreply.github.com>
2020-11-19 07:41:07 -0400

    Method parser

--- parse.java
+++ parse.java
@@ -1,11 +1,19 @@
 public class parse {
     public static void main(String[] args){
+        //variable double and float
         System.out.println("Parse String to Number");
-        String data = "22";
-        int convert = Integer.parseInt(data);
-        System.out.println("String:'22'\nNumber:"+convert);
+        String text = "22";
+        int number = Integer.parseInt(text);
+        System.out.println("String:'22'\nNumber:"+number);
         //number to string
         System.out.println("Parse Number to String");
-        
+        number = 1;
+        text = String.valueOf(number);
+        System.out.println("Number:1\nString:'"+text+"'");
+        //number to double
+        System.out.println("Parse Number to Double");
+        number = 33;
+        float f = number;
+        System.out.println("Number:"+number+"\nFloat:"+f);
     }
 }
