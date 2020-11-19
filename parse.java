044822f63e43a5515a4670b8052f2784a29a97a2
RogerCMD <74694552+RogerCMD@users.noreply.github.com>
2020-11-18 23:14:41 -0400

    Parse data in java
    this is difference method for parser in java

---
+++ parse.java
@@ -0,0 +1,11 @@
+public class parse {
+    public static void main(String[] args){
+        System.out.println("Parse String to Number");
+        String data = "22";
+        int convert = Integer.parseInt(data);
+        System.out.println("String:'22'\nNumber:"+convert);
+        //number to string
+        System.out.println("Parse Number to String");
+        
+    }
+}
